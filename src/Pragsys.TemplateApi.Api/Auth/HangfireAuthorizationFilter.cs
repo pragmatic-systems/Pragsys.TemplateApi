2a01a29cc42bb47aa9be815cada3d13c1bd827a1
Tristan Rhodes <tristanjerhodes@yahoo.co.uk>
2026-06-09 22:49:54 +0100

    Formatting

--- src/Pragsys.TemplateApi.Api/Auth/HangfireAuthorizationFilter.cs
+++ src/Pragsys.TemplateApi.Api/Auth/HangfireAuthorizationFilter.cs
@@ -1,11 +1,11 @@
-﻿using Hangfire.Dashboard;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using System;
+﻿using System;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Hangfire.Dashboard;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace Pragsys.TemplateApi.Api.Auth;
 
@@ -14,8 +14,7 @@ public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
     public bool Authorize(DashboardContext context)
     {
         // TODO: Get the context and validate JWT and permission for Hangfire.
-        //var httpContext = context.GetHttpContext();
-
+        // var httpContext = context.GetHttpContext();
         return true;
     }
 }
